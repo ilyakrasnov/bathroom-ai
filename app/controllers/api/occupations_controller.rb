adfe53b982a0ab367887631a96b73412493dd783
Ilya <ilya.krasnov@gmail.com>
2018-06-17 14:13:08 +0300

    Hide room id from api response

--- app/controllers/api/occupations_controller.rb
+++ app/controllers/api/occupations_controller.rb
@@ -1,7 +1,7 @@
 class Api::OccupationsController < ApplicationController
 
   def index
-    @rooms = Room.select("id, name, occupied").all
+    @rooms = Room.select("name, occupied").all
     respond_to do |format|
       format.json { render :json => @rooms }
     end
