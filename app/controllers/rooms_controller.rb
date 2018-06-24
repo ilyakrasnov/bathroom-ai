86d0009fdd27f314f5c5bf764c4395ce4cca9783
Ilya <ilya.krasnov@gmail.com>
2018-06-24 21:56:18 +0300

    Add basic auth for the website

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,7 @@
 class RoomsController < ApplicationController
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  http_basic_authenticate_with name:ENV['user'], password: ENV['password']
+
   def index
     @rooms = Room.all
   end
